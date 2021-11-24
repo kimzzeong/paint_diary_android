1cd9b0d519201b49fe40dc8c29822a74c1e515a8
kimzzeong <74105034+kimzzeong@users.noreply.github.com>
2021-11-25 01:02:13 +0900

    채팅방 레이아웃 만듦

---
+++ app/src/main/java/com/example/paint_diary/Adapter/RoomAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.paint_diary.Adapter
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.paint_diary.Data.Room
+import com.example.paint_diary.R
+import java.util.ArrayList
+
+class RoomAdapter : RecyclerView.Adapter<RoomAdapter.ViewHolder>() {
+    var roomList = mutableListOf<Room>()
+    var context : Context? = null
+    var user_idx : Int = 0 //유저 고유 번호
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        val sharedPreferences = context?.getSharedPreferences("user",0)
+        user_idx = Integer.parseInt(sharedPreferences?.getString("user_idx", "0"))
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.chat_room, parent, false)
+        Log.e("onCreateViewHolder","onCreateViewHolder")
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        Log.e("onBindViewHolder","onBindViewHolder")
+        holder.bind(roomList.get(position))
+
+
+    }
+
+    override fun getItemCount(): Int {
+        Log.e("getItemCount","getItemCount")
+        return roomList.size
+    }
+
+
+    class ViewHolder (itemView: View):RecyclerView.ViewHolder(itemView){
+
+        var chat_profile : ImageView = itemView.findViewById(R.id.chat_profile)
+        var chat_nickname : TextView = itemView.findViewById(R.id.chat_nickname)
+        var chat_datetime : TextView = itemView.findViewById(R.id.chat_datetime)
+        var chat_content : TextView = itemView.findViewById(R.id.chat_content)
+
+        fun bind(item: Room){
+            chat_nickname.text = item.room_name
+            chat_datetime.text = item.room_datetime
+            chat_content.text = item.message
+//            var uriToString : String = item.room_profilePhoto
+//            if(uriToString != null){
+//
+//                var uri : Uri = Uri.parse(uriToString)
+//                var uri_diary = "http://3.36.52.195/profile/"+uri
+//                Glide.with(itemView).load(uri_diary).into(chat_profile)
+//            }else{
+                chat_profile.setImageResource(R.drawable.basic_profile)
+           // }
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/paint_diary/Fragment/ChatFragment.kt
+++ app/src/main/java/com/example/paint_diary/Fragment/ChatFragment.kt
@@ -7,9 +7,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.paint_diary.Adapter.RoomAdapter
+import com.example.paint_diary.Adapter.UserProfileDiaryListAdapter
+import com.example.paint_diary.Data.Room
 import com.example.paint_diary.R
 import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.android.synthetic.main.fragment_mypage.*
+import java.util.ArrayList
 
 class ChatFragment : Fragment() {
     companion object {
@@ -18,6 +26,9 @@ class ChatFragment : Fragment() {
             return ChatFragment()
         }
     }
+
+    lateinit var roomAdapter: RoomAdapter
+    var roomList = mutableListOf<Room>()
 
     //메모리에 올라갔을 때
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,20 +58,36 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val activity = activity as AppCompatActivity?
+
+
         //메뉴이름 변경 해야함
         //chat_toolbar.inflateMenu(R.menu.home_menu)
         chat_toolbar.setTitleTextColor(Color.WHITE)
         chat_toolbar.setTitle("채팅")
+
+        roomAdapter = RoomAdapter()
+        val layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
+        chatroom_recyclerview.layoutManager = layoutManager
+
+
+        //var room = Room("테스트 날짜","테스트 메세지","테스트 방 이름","https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201909/10/97737cb1-a4b9-4ee2-a5b7-f784540cfd30.jpg")
+        //roomList!!.add(room)
+        chatroom_recyclerview.adapter = roomAdapter
+        //roomAdapter.roomList = roomList!!
+        //Log.e("roomlist",""+roomList?.size)
+
+
+
+        roomList.apply {
+            add(Room("테스트 날짜", "테스트 메세지", "테스트 방 이름", "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201909/10/97737cb1-a4b9-4ee2-a5b7-f784540cfd30.jpg"))
+
+            roomAdapter.roomList = roomList
+            roomAdapter.notifyDataSetChanged()
+
+        }
     }
 
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return when (item.itemId) {
-//            R.id.date_pick-> {
-//                // navigate to settings screen
-//                true
-//            }
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
 }
